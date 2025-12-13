0a0d61d062be2cfe4c701a08c574cba47fe6f81b
AMUL SHARMA <140779732+CODE-WITH-AMUL@users.noreply.github.com>
2025-12-14 00:08:25 +0545

    Config Code

---
+++ core/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
+
+
+def index(request):
+	return render(request,'index.html')
+
+
+@login_required
+def home(request):
+    return render(request,'home.html')
+
